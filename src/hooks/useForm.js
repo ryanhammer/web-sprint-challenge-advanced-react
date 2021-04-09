a2a5ba75d7e624e3cf628d7b250624d82a4b9047
rhammer35 <rhammer35@icloud.com>
2021-04-09 15:15:58 -0400

    updated comments in useForm custom hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,9 +1,11 @@
 // write your custom hook here to control your checkout form
 import useLocalStorage from './useLocalStorage';
 
+// uselocalstorage custom hook uses localstorage to keep form state variable values stored until user clears form (or browser times out of maintaining local storage)
 const useForm = (initialValues) => {
   const [formValues, setFormValues] = useLocalStorage('checkoutForm', initialValues);
 
+  // form values are updated each time input fields are updated using state variable and spread operator
   const handleChanges = e => {
     setFormValues({
       ...formValues,
@@ -11,6 +13,7 @@ const useForm = (initialValues) => {
     });
   };
 
+  // User can clear form through addition of Clear Form button and event handler below that resets formvalues state variable and local storage to initial values
   const clearForm = e => {
     e.preventDefault();
     setFormValues(initialValues);
