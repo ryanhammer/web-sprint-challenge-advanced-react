8a12d251771036724ed53ddea8c4f48d96d5c245
rhammer35 <rhammer35@icloud.com>
2021-04-09 15:12:20 -0400

    updated comments in uselocalstorage custom hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 
+// useLocalStorage will employ use of localStorage to organize form stateful logic and maintain form values if page is refreshed or form is submitted
+// A clear form button is added to allow users to clear out values if they want/need to start over
 const useLocalStorage = (key, initialValue) => {
-  // When we create state we check to see if that value is in localStorage
-  // If it does, put that into our state value
-  // If it doesn't, put our initialValue into the state AND localStorage
-  // When we update our state, save that update to localStorage
+  // Check to see if value is in localStorage then if it is, insert that into our state value. If it isn't, put the initial value into state value and into localStorage
+
 
   const [value, setValue] = useState( () => {
       if (window.localStorage.getItem(key)) {
@@ -13,7 +13,8 @@ const useLocalStorage = (key, initialValue) => {
       window.localStorage.setItem(key, JSON.stringify(initialValue));
       return initialValue;
     });
-
+    
+    // Any update of state is saved into localStorage
     const setLocalStorageValue = value => {
       window.localStorage.setItem(key, JSON.stringify(value));
       setValue(value);
