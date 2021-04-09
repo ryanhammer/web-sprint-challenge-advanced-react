c4532f2ada1d8d30f8df44eeea64460f5b2ba2d6
rhammer35 <rhammer35@icloud.com>
2021-04-09 13:22:54 -0400

    created state variable for plants and completed axios call code to properly display plants in plantList.js

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,7 +3,9 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  state = {
+    plants: []
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
@@ -12,7 +14,10 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        console.log(res);
+        this.setState({
+          plants: res.data
+        });
+        console.log(this.state.plants);
       });
   }
 
